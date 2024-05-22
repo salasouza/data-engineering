410234cfa21d595d0f6b4c252424c6b9a976ee57
Salatiel Souza <sal.souzasa@gmail.com>
2024-05-21 21:36:28 -0300

    :sparkles: new feature

--- src/beam/pipelines/pipeline.py
+++ src/beam/pipelines/pipeline.py
@@ -7,15 +7,17 @@ with beam.Pipeline() as p_1:
 
     consulta = (
         p_1 
-        |"Read database" >> beam.io.ReadFromText("database/raw/iris.csv", skip_header_lines = 1)
+        |"Read database" >> beam.io.ReadFromText("database/raw/consulta.txt", skip_header_lines = 1) # Read Transform gerando Pcoll 
         |"Transform" >> beam.Map(lambda record: record.split(','))
-        |"Filter values" >> beam.Filter(lambda record: float(record[2]) > 2)
+        # |"Filter values" >> beam.Filter(lambda record: float(record[2]) > 2)
+        |"Filter 1">> beam.Filter(lambda record: float(record[2]) < 10)
+        |"Filter 2">> beam.Filter(lambda record: str(record[1])=='Finalizado')
         |"Show database" >> beam.Map(print)
-        |"Save database" >> beam.io.WriteToText('database/trusted/iris_test.csv')    
+        |"Save database" >> beam.io.WriteToText('database/trusted/consulta_test.txt')    # Write transform gerando Pcoll
         )
     
     if consulta:
         p_1.run()
-        print("Operation Success!")
+        print("\nOperation Success!\n")
     else:
-        print("Operation Failed!")
+        print("\nOperation Failed!\n")
